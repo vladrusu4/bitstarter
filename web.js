f6cba3ed05e99af1695b401950276a746e56e530
Vlad <vlad.rusu04@yahoo.com>
2013-07-04 12:26:45 +0000

    updated web.js

--- web.js
+++ web.js
@@ -5,11 +5,10 @@ var http = require('http');
 var fs = require('fs');
 var index = "index.html";
 
-var buf = new Buffer(256);
-var len = buf.write("Hello World 2!", 0);
-
-
-fs.readFileSync(index, "binary", function(err, file) {
+app.get(index, function(request, response) {
+    var buf = new Buffer(256);
+    var len = buf.write("Hello World 2!", 0);
+    fs.readFileSync(index, "binary", function(err, file) {
 	if(err) {
 		response.writeHead(500, {"Content-Type": "text/plain"});
 		response.write(err + "\n");
@@ -19,8 +18,8 @@ fs.readFileSync(index, "binary", function(err, file) {
 	response.writeHead(200);
 	response.write(file, buf.toString('utf8', 0, len));
 	response.end();
+    });
 });
-
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
@@ -29,5 +28,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
