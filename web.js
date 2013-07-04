0d2c5010f08d7e98159455883ce8f32339fb3062
Vlad <vlad.rusu04@yahoo.com>
2013-07-04 13:45:49 +0000

    bitstarter index

--- web.js
+++ web.js
@@ -1,19 +1,14 @@
-var express = require('express');
-var app = express.createServer(express.logger());
-var http = require('http');
-var fs = require('fs');
-
-app.get('/index.html', function(request, response) {
-    var buf = new Buffer(256);
-	var data = fs.readFileSync("./index.html", "utf8").toString();
-    var len = buf.write(data, 0);
-//	response.writeHead(200);
-	response.write(buf.toString('utf8', 0, len));
-	response.end();
-});
+var express = require('express'),
+	app = express.createServer(express.logger()),
+	http = require('http'),
+	fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buf = new Buffer(256);   
+	var len = buf.write(fs.readFileSync("./index.html", "utf8").toString(), 0);
+	response.writeHead(200);
+	response.write(file, buf.toString('utf8', 0, len));
+	response.end();
 });
 
 var port = process.env.PORT || 8000;
